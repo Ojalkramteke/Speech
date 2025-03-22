8ba3869c1dffa6a24398530008e554868a12e06e
Ojalkramteke <ojalramteke@gmail.com>
2025-03-22 17:01:51 +0530

    final

--- speech_to_text.py
+++ speech_to_text.py
@@ -7,22 +7,30 @@ for voice in voices:
     engine.setProperty('voice', voice.id)
     engine.setProperty("rate", 175)
 
-def speak(command):
-    engine.say(command)
+def speak(audio):
+    engine.say(audio)
     engine.runAndWait()
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+def command():
+    content = " "
+    while content == " ":
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Say something!")
+            audio = r.listen(source)
 
-# recognize speech using Google Speech Recognition
-try:
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        # recognize speech using Google Speech Recognition
+        try:
+            content = r.recognize_google(audio, language='en-in')
+            print("You said ........." + content)
+        except Exception as e:
+            print("Please try again...")
 
+    return content
+
+def main_process():
+    while True:
+        request = command()
+        print(request)
 #speak("How are you?")
